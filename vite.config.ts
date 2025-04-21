b5fea823f7daf0e9602e05d04a0036b6987ee58b
Joshua sunday <joshuasunday300@gmail.com>
2025-04-21 23:11:10 +0100

    fixed locally, i hope it works =(

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,54 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+// import path from 'path';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   define: {
+//     'process.env': {},
+//     'global': 'globalThis',
+//   },
+//   plugins: [ tailwindcss(), react()],
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       define: {
+//         global: 'globalThis',
+//       },
+//       plugins: [
+//         NodeGlobalsPolyfillPlugin({
+//           buffer: true,
+//         }),
+//       ],
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       buffer: 'buffer', 
+//       'node:fs': path.resolve(__dirname, 'src/fs-mock.js'),
+//     },
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import path from 'path';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import inject from '@rollup/plugin-inject'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   define: {
     'process.env': {},
-    'global': 'globalThis',
+    global: 'globalThis',
   },
-  plugins: [ tailwindcss(), react()],
+  plugins: [
+    tailwindcss(), 
+    react(),
+  ],
   optimizeDeps: {
     esbuildOptions: {
       define: {
@@ -18,14 +56,25 @@ export default defineConfig({
       },
       plugins: [
         NodeGlobalsPolyfillPlugin({
+          process: true,
           buffer: true,
+        }),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        inject({
+          Buffer: ['buffer', 'Buffer'],
         }),
       ],
     },
   },
   resolve: {
     alias: {
-      buffer: 'buffer', 
+      buffer: 'buffer',
+      'node:buffer': 'buffer',
       'node:fs': path.resolve(__dirname, 'src/fs-mock.js'),
     },
   },
